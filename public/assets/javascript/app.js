e0f4ebc7fc1ac3d8d8663f403da35d76e8725296
Nathan Rendon <naterendon@gmail.com>
2020-01-25 22:04:06 -0600

    organized code

---
+++ public/assets/javascript/app.js
@@ -0,0 +1,26 @@
+ // Save Article Buttone
+ $(".save").on("click", function() {
+    // Save the id from the save tag
+    var thisId = $(this).attr("data-id");
+
+    $.ajax({
+      method: "POST",
+      url: "/article/" + thisId
+    })
+      .then(function(data) {
+        console.log("updated");
+      });
+  });
+
+  $(".delArticle").on("click", function() {
+    // Save the id from the save tag
+    var thisId = $(this).attr("data-id");
+
+    $.ajax({
+    method: "POST",
+    url: "/article/delete/" + thisId
+    })
+    .then(function(data) {
+        console.log("deleted");
+    });
+});
